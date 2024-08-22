221b7754d26142875cea942bfe5139118dcad2ca
Jonathan Neidel <jneidel@jneidel.com>
2024-08-22 14:02:44 +0200

    Create script to move newsletter signup above footnotes

---
+++ assets/js/custom.js
@@ -0,0 +1,21 @@
+(function move_newsletter_signup_above_footnotes() {
+  function get_footnotes() {
+    const referencesSection = document.querySelector("#referenzen") || document.querySelector("#references");
+    if (referencesSection) return referencesSection;
+
+    return document.querySelector(".footnotes");
+  }
+  function get_last_newsletter_signup() {
+    const forms = Array.from( document.querySelectorAll(".newsletter-signup") );
+    const lastForm = forms[forms.length-1];
+    return lastForm;
+  }
+
+  const footnotes = get_footnotes();
+  const signupForm = get_last_newsletter_signup();
+
+  if (footnotes && signupForm) {
+    const parentOfFootnotes = footnotes.parentNode;
+    parentOfFootnotes.insertBefore(signupForm, footnotes);
+  }
+})()
